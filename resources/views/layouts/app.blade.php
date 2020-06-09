e7b8cbdb11c1d9d568d246bcab25cbd54bb6098b
True Digital Limited <info@true-digital.eu>
2020-06-09 09:26:02 +0200

    clean

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>iamcars</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.min.css') }}">
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/script.min.js') }}"></script>
+
+
+    <meta name="keywords" content="">
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
